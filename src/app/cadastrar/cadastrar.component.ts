5de1b0642a0f989af55a644fe55ee0363555e5af
Sarah Nani <sarahlenani@gmail.com>
2022-02-25 15:52:08 -0300

    @sarahnani [Adicionando formulários diferentes por tipo de usuário]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
 
 @Component({
   selector: 'app-cadastrar',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar.component.css']
 })
 export class CadastrarComponent implements OnInit {
+
+  usuario: Usuario = new Usuario
 
   constructor() { }
 
