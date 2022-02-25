e839151defb3fddde9cac43d7f7a2005825f1807
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-02-25 11:55:14 -0300

    @BruRaptor [Adicionando Model e Componente Inicio]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatoComponent } from './contato/contato.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {path:'' , redirectTo:'entrar' , pathMatch :'full'},
   {path:'entrar', component: EntrarComponent},
   {path:'contato', component:ContatoComponent},
-  {path:'sobre', component:SobreComponent}
+  {path:'sobre', component:SobreComponent},
+  {path:'inicio', component:InicioComponent},
 ];
 
 @NgModule({
